361ff4fd0506ca9595bed95792949c5d8d500c29
Brian Gerhold <brian.gerhold@gmail.com>
2020-08-02 10:01:52 -0700

    Partially effective consistent / DRY refactor; tracking points incorrect on slide2;

--- narrativeViz.js
+++ narrativeViz.js
@@ -8,23 +8,21 @@ const canvasWidth  = totalCanvasWidth - canvasMargin.left - canvasMargin.right
 const canvasHeight = totalCanvasHeight  - canvasMargin.top - canvasMargin.bottom
 
 const tempColor          = 'DarkRed'
-const trackingLineClass  = 'tracking-line'
-const lineDataPointClass = 'line-data-point'
+const lineClass          = 'line'
 	
 function sleep(ms)      { return new Promise(resolve => setTimeout(resolve, ms)) }
 function jqEltId(eltId) { return '#' + eltId }
 function classId(name)  { return '.' + name }
+function lineId(slideInfo, lineIndex)          { return slideInfo.lineIdPrefix + lineIndex }
 function lineDataPointId(slideInfo, lineIndex) { return slideInfo.linePointIdPrefix + lineIndex }
 
 function parseId(elt, indexIndex)      { return parseInt(elt.attr('id').substr(indexIndex)) }
 function parseLineId(elt)              { return parseId(elt, 6) }
-function parseLegendId(elt)            { return parseId(elt, 8) }
 function parseLineIdFromDataPoint(elt) { return parseInt(elt.attr('id').substr(11,12))}
-function parseMouseOverId(elt)         { return parseInt(elt.attr('id').substr(0,1))}
 
 function setDataPointVisibility(slideInfo, visible) {
-  d3.select(classId(trackingLineClass)).style('opacity', visible ? '1' : '0')
-  for (var i=0; i<slideInfo.lineCount; ++i) {
+  d3.select(jqEltId(slideInfo.trackingLineId)).style('opacity', visible ? '1' : '0')
+  for (var i=0; i<slideInfo.dataObjs.length; ++i) {
     d3.select(jqEltId(lineDataPointId(slideInfo, i))).select('circle').style('opacity', visible ? '1' : '0')
     d3.select(jqEltId(lineDataPointId(slideInfo, i))).select('text').style('opacity', visible ? '1' : '0')
   }
@@ -37,12 +35,109 @@ for (var i = 0; i<slideCount; ++i) {
     id:                      slideId,
     lineIdPrefix:            slideId + 'line',
     linePointIdPrefix:       slideId + 'linePoint',
-    mouseoverRegionIdSuffix: slideId + 'mouseOver'    
+    trackingLineId:          slideId + 'tracking-line'
   }
+  var slideDiv = d3.select(jqEltId(slideInfo.id))
+  slideDiv.style('display','none')
+  slideInfo.svg = slideDiv
+    .append('svg').attr('width', canvasWidth + canvasMargin.left + canvasMargin.right).attr('height', canvasHeight + canvasMargin.top + canvasMargin.bottom)
+    .append('g').attr('transform', 'translate(' + canvasMargin.left + ',' + canvasMargin.top + ')')
   slideInfos[i] = slideInfo
-  //window['buildSlide' + i](slideInfo);
 }
 
-buildSlide1(slideInfos[0])
-//buildSlide2(slideInfos[1])
-//buildSlide3(slideInfos[2])
+async function populateDataObjs(slideInfo, dataFiles, lineColors, sliceCounts) {
+  slideInfo.lineCount = dataFiles.length
+  var minYear     = 0
+  var maxYear     = 0
+  slideInfo.dataObjs = []
+  for (var i=0; i<slideInfo.lineCount; ++i) {
+    slideInfo.dataObjs[i] = {}
+    slideInfo.dataObjs[i].values = await d3.csv('/data/' + dataFiles[i] + '.csv')
+    slideInfo.dataObjs[i].values = slideInfo.dataObjs[i].values.slice(sliceCounts[i])
+    slideInfo.dataObjs[i].color  = lineColors[i]
+    slideInfo.dataObjs[i].y      = null
+    var dataMinYear = d3.min(slideInfo.dataObjs[i].values, function(d) { return d.year; })
+    var dataMaxYear = d3.max(slideInfo.dataObjs[i].values, function(d) { return d.year; })
+    if (minYear == 0 || minYear > dataMinYear ) minYear = dataMinYear
+    if (maxYear == 0 || maxYear < dataMaxYear ) maxYear = dataMaxYear
+  }
+  slideInfo.minYear = minYear
+  slideInfo.maxYear = maxYear
+}
+
+function addDataLines(slideInfo) {
+  for (var i=0; i<slideInfo.lineCount; ++i) {
+    slideInfo.svg.append('path')
+      .datum(slideInfo.dataObjs[i].values)
+      .attr('class', 'line')
+      .attr('id', lineId(slideInfo,i))
+      .attr('stroke-linecap', 'round')
+      .attr('stroke-width', 2)
+      .attr('stroke', slideInfo.dataObjs[i].color)
+      .attr('d', d3.line().x(function(d) { return  slideInfo.dataObjs[i].x(d.year) }).y(function(d) { return slideInfo.dataObjs[i].y(+d.value) }))
+  }
+}
+
+function addMouseOverEffects(slideInfo) {
+  var mouseG = slideInfo.svg.append('g')
+  for (var i=0; i<slideInfo.dataObjs.length; ++i) {
+    var lineDataPoint = mouseG.append('g').attr('id', lineDataPointId(slideInfo, i))
+    lineDataPoint.append('circle')
+      .attr('r', 5)
+      .style('stroke', slideInfo.dataObjs[i].color)
+      .style('fill', 'none')
+      .style('stroke-width', '1px')
+      .style('opacity', '0')
+    lineDataPoint.append('text').attr('stroke', slideInfo.dataObjs[i].color).attr('transform', 'translate(-15,-10)');
+  }
+	
+  //mouse-tracking vertical line
+  mouseG.append('path').attr('id', slideInfo.trackingLineId).style('stroke', 'Lavender').style('stroke-width', '1px').style('opacity', '0');
+  var lines = document.getElementsByClassName(lineClass);
+  mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
+    .attr('width', canvasWidth) // can't catch mouse events on a g element
+    .attr('height', canvasHeight)
+    .attr('fill', 'none')
+    .attr('pointer-events', 'all')
+    .on('mouseover', function() { setDataPointVisibility(slideInfo, true) })
+    .on('mouseout',  function() { setDataPointVisibility(slideInfo, false) })
+    .on('mousemove', function() {
+      var mouse = d3.mouse(this);
+      d3.select(jqEltId(slideInfo.trackingLineId)).attr('d', function() {
+        var d = "M" + mouse[0] + "," + canvasHeight;
+        d += " " + mouse[0] + "," + 0;
+        return d;
+      });
+    
+      for (var i =0; i<slideInfo.dataObjs.length; ++i ) {
+        var pointEltId = jqEltId(lineDataPointId(slideInfo, i)) 
+        d3.select(pointEltId)
+          .attr('transform', function() {
+            var beginning = 0
+            var end       = lines[i].getTotalLength()
+            var target    = null
+            var pos
+            while (true) {
+              target = Math.floor((beginning + end) / 2);
+              pos = lines[i].getPointAtLength(target);
+              if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+                  break;
+              }
+              if (pos.x > mouse[0])      end = target;
+              else if (pos.x < mouse[0]) beginning = target;
+              else break; //position found
+            }
+
+            d3.select(pointEltId).select('text').text(slideInfo.dataObjs[i].y.invert(pos.y).toFixed(2));
+
+            return 'translate(' + mouse[0] + ',' + pos.y + ')';
+          });
+      }
+    });
+}
+
+buildSlide1(slideInfos[0]); //d3.select(jqEltId(slideInfos[0].id)).style('display','block')
+buildSlide2(slideInfos[1]); d3.select(jqEltId(slideInfos[1].id)).style('display','block')
+buildSlide3(slideInfos[2]);
+
+//for (var i = 0; i<slideCount; ++i) { window['buildSlide' + i](slideInfo); }
--- slide2.js
+++ slide2.js
@@ -5,9 +5,10 @@ async function buildSlide2(slideInfo) {
   function unselectedLineColor(index) { return index == 0 ? colors[index] : 'lightgrey' }
   function unselectedLineWidth(index) { return index == 0 ? 2 : 1 }
   function animationTime(index)       { return index == 0 ? 5000 : 2000 }
-  function lineSelectId(index)        { return '#' + slideInfo.lineIdPrefix + index }
-  function legendSelectId(index)      { return '#' + legendIdPrefix + index }
-  
+  function lineSelectId(index)        { return jqEltId(slideInfo.lineIdPrefix + index) }
+  function legendSelectId(index)      { return jqEltId(legendIdPrefix + index) }
+  function parseLegendId(elt)         { return parseId(elt, 8) }
+
   function selectLine(index) {
     var color = colors[index]
     d3.select(lineSelectId(index)).style('stroke-width', 3).attr('stroke', color)
@@ -42,55 +43,39 @@ async function buildSlide2(slideInfo) {
       .style('stroke-width', unselectedLineWidth(index))
   }
 
-  // append the svg object to the body of the page
-  var svg = d3.select(jqEltId(slideInfo.id))
-    .append('svg').attr('width', canvasWidth + canvasMargin.left + canvasMargin.right).attr('height', canvasHeight + canvasMargin.top + canvasMargin.bottom)
-    .append('g').attr('transform', 'translate(' + canvasMargin.left + ',' + canvasMargin.top + ')')
+  var dataFiles   = ['Nasa Global Temp Change', '1975 Broecker', '1981 Hansen', '1988 Hansen', '1990 Ipcc1',  '1995 Ipcc2', '2001 Ipcc3', '2007 Ipcc4',  '2013 Ipcc5']
+  var colors      = [tempColor,                 'Indigo',        'SaddleBrown', 'Sienna',      'DarkBlue',    'MediumBlue', 'Blue',       'DeepSkyBlue', 'LightSkyBlue']
+  var sliceCounts = [90,                        0,               0,             0,             0,             0,            0,            0,             0]
+  
+  await populateDataObjs(slideInfo, dataFiles, colors, sliceCounts)
 
-  var dataFiles  = ['Nasa Global Temp Change', '1975 Broecker', '1981 Hansen', '1988 Hansen', '1990 Ipcc1',  '1995 Ipcc2', '2001 Ipcc3', '2007 Ipcc4',  '2013 Ipcc5']
-  var colors     = [tempColor,                 'Indigo',        'SaddleBrown', 'Sienna',      'DarkBlue',    'MediumBlue', 'Blue',       'DeepSkyBlue', 'LightSkyBlue']
-  var allData    = []
-  
-  for (var i=0; i<dataFiles.length; ++i) {
-	  allData[i] = await d3.csv('/data/' + dataFiles[i] + '.csv')
-  }
-  
-  allData[0]     = allData[0].slice(90)
-  var yearMins   = allData.map( data => d3.min(data, function(d) { return d.year; }))
-  var yearMaxes  = allData.map( data => d3.max(data, function(d) { return d.year; }))
-  var deltaMins  = allData.map( data => d3.min(data, function(d) { return +d.value; }))
-  var deltaMaxes = allData.map( data => d3.max(data, function(d) { return +d.value; }))
+  var valueMins  = slideInfo.dataObjs.map( dataObj => d3.min(dataObj.values, function(d) { return +d.value; }))
+  var valueMaxes = slideInfo.dataObjs.map( dataObj => d3.max(dataObj.values, function(d) { return +d.value; }))
 
   //x-axis
-  var yearsMinMax = [Math.min(...yearMins), Math.max(...yearMaxes)]
-  var x = d3.scaleLinear().domain(yearsMinMax).range([ 0, canvasWidth ])
-  svg.append('g').attr('transform', 'translate(0,' + canvasHeight + ')').call(d3.axisBottom(x).tickFormat(d3.format('d')));
+  var x = d3.scaleLinear().domain([slideInfo.minYear, slideInfo.maxYear]).range([ 0, canvasWidth ])
+  slideInfo.svg.append('g').attr('transform', 'translate(0,' + canvasHeight + ')').call(d3.axisBottom(x).tickFormat(d3.format('d')));
 
   //y-axis
-  var dataMinMax = [Math.min(...deltaMins) - 0.1, Math.max(...deltaMaxes)]
+  var dataMinMax = [Math.min(...valueMins) - 0.1, Math.max(...valueMaxes)]
   var y = d3.scaleLinear().domain(dataMinMax).range([ canvasHeight, 0 ]);
-  svg.append('g').call(d3.axisLeft(y))
-  svg.append('text').attr('transform', 'rotate(-90)').attr("y", 0 - canvasMargin.left)
+  slideInfo.svg.append('g').call(d3.axisLeft(y))
+  slideInfo.svg.append('text').attr('transform', 'rotate(-90)').attr("y", 0 - canvasMargin.left)
     .attr("x",0 - (canvasHeight / 2))
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .style('font-size', '70%')
     .text('Global temperature change \u00B0C')
 
-  //tooltip
-  const tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0)
-    .style("position", "absolute");  
+  for (var i=0; i<slideInfo.lineCount; ++i) {
+	slideInfo.dataObjs[i].x = x
+    slideInfo.dataObjs[i].y = y
 
-  var tooltipPoint = svg.append('circle').attr('cx', 0).attr('cy', 0).attr('r', 5).attr('id', tooltipPointId).style('opacity', 0)
-  
-  for (var i=0; i<dataFiles.length; ++i) {
-    var lineName = (i == 0 ? 'Actual' : dataFiles[i].substr(5) )
+    /*
     //line
-    svg.append('path')
-      .datum(allData[i])
-      .attr('class', 'line')
+    slideInfo.svg.append('path')
+      .datum(slideInfo.dataObjs[i].values)
+      .attr('class', lineClass)
       .attr('id', slideInfo.lineIdPrefix + i)
       .attr('stroke-linecap', 'round')
       .attr('stroke-width', unselectedLineWidth(i))
@@ -98,37 +83,10 @@ async function buildSlide2(slideInfo) {
       .attr('d', d3.line().x(function(d) { return x(d.year) }).y(function(d) { return y(d.value) }))
       .on('mouseover', function(d) { selectLine(  parseLegendId(d3.select(this))) })
       .on('mouseout',  function(d) { unselectLine(parseLegendId(d3.select(this))) })
-
-    //mouseover regions
-    svg.selectAll('circles')
-      .data(allData[i])
-      .enter()
-      .append('circle')
-      .attr('cx', function(d) { return x(d.year) })      
-      .attr('cy', function(d) { return y(d.value) })
-      .attr('r', 10)
-      .attr('id', function(d) { return i + '_' + d.i + '_' + slideInfo.mouseoverRegionIdSuffix })
-      .style('opacity', 0)
-      .on('mouseover', function(d) {
-        var thisCircle = d3.select(this)
-        var lineIndex = parseMouseOverId(thisCircle)
-        
-        tooltip.transition().delay(30).duration(200).style('opacity', 1);
-        tooltip.html('<span>' + d.year + ':</span><br/><span>+' + d.value + '</span>').style('left', (x(d.year) + 50) + 'px').style("top", (y(d.value) - 20) + 'px').style('color', colors[lineIndex])
-        tooltipPoint.attr('cx', thisCircle.attr('cx')).attr('cy', thisCircle.attr('cy'))
-        tooltipPoint.transition().delay('20').duration('200').style('opacity', 1).style('fill', colors[lineIndex])    
-        selectLine(lineIndex)
-      })
-      .on('mouseout', function(d) {
-        var thisCircle = d3.select(this)
-        var lineIndex = parseMouseOverId(thisCircle)
-        tooltip.transition().duration(100).style('opacity', 0)
-        tooltipPoint.transition().delay('20').duration('200').style('opacity', 0)
-        unselectLine(lineIndex)
-      })
+      */
 
     //legend
-    svg.append('text')
+    slideInfo.svg.append('text')
       .attr('class', 'legend')
       .attr('id', legendIdPrefix + i)
       .attr('x', 20)
@@ -141,9 +99,14 @@ async function buildSlide2(slideInfo) {
   
   /*
   d3.selectAll('.line').style('opacity', '0')
-  for (var i=0; i<dataFiles.length; ++i) {
+  for (var i=0; i<slideInfo.lineCount; ++i) {
     animateLine(i)
     await sleep(animationTime(i))
   }
   */
+
+  addDataLines(slideInfo)
+  addMouseOverEffects(slideInfo)
+  
+  for (var i=0; i<slideInfo.lineCount; ++i) {unselectLine(i)}
 }
